cf45c52bd8859c4d12ec69836f9aaeea38343018
wans <17340310@qq.com>
2017-02-22 11:14:46 +0800

    Initial commit

---
+++ src/com/broad/bean/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.broad.bean;
+
+/**
+ * Created by Administrator on 17-2-22.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World! IntelliJ");
+    }
+}
